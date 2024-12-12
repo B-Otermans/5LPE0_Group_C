f8c7537655f40d97064e0f18079c06b7323500f5
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2024-12-12 09:55:42 +0100

    bewerkt voor antenna classes

--- setup_controls.py
+++ setup_controls.py
@@ -1,12 +1,18 @@
 import antennas
 import utils
+import imp
 
 
+# force module updates
 CUSTOM_MODULES = [antennas, utils]
+for module in CUSTOM_MODULES:
+    imp.reload(module)
+
 CLEAR_LIST = ["Fractionated Dipole Array"]
 
 # array setup parameters
-N_DIPOLES = 12
+ARRAY_NAME = "Fractionated Dipole Array"
+N_DIPOLES = 4
 DIPOLE_SETTINGS = {"length": 250,
                    "width": 10,
                    "gapwidth": 2,
@@ -17,14 +23,12 @@ ARRAY_WIDTH = 240
 ARRAY_HEIGHT = 300
 
 if __name__ == "__main__":
-    # force module updates
-    utils.update_modules(CUSTOM_MODULES)
     # remove old or duplicate entity groups
     utils.clear_from_model(CLEAR_LIST)
     # instantiate array
-    frac_dipole_array = antennas.elipse_array(n_antennas=N_DIPOLES,
-                                              antenna_parameters=DIPOLE_SETTINGS,
-                                              antenna_class=FRACTIONATED_DIPOLE_CLASS,
-                                              array_width=ARRAY_WIDTH,
-                                              array_height=ARRAY_HEIGHT)
-    frac_dipole_array.Name = "Fractionated Dipole Array"
+    frac_dipole_array = antennas.ElipseArray(name=ARRAY_NAME,
+                                             n_antennas=N_DIPOLES,
+                                             antenna_parameters=DIPOLE_SETTINGS,
+                                             antenna_class=FRACTIONATED_DIPOLE_CLASS,
+                                             array_width=ARRAY_WIDTH,
+                                             array_height=ARRAY_HEIGHT)
