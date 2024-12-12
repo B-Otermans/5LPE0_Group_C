6a6332e67c46df2bb51060bea7bc8b180eb9139e
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2024-12-12 09:56:46 +0100

    update voor array class

--- simulation_controls.py
+++ simulation_controls.py
@@ -1,14 +1,16 @@
 import simulate
 import utils
 import setup_controls
+import imp
 
-
+# force module updates
 CUSTOM_MODULES = [simulate, utils, setup_controls]
+for module in CUSTOM_MODULES:
+    imp.reload(module)
+
 SIMULATION_GROUPS = ["Fractionated Dipole Array"]
 
 
 if __name__ == "__main__":
-    # force module updates
-    utils.update_modules(CUSTOM_MODULES)
     # run multiport simulation
-    simulate.multiport_sim(setup_controls.frac_dipole_array, frequency=298)
+    simulate.multiport_sim(frac_dipole_array, frequency=298)
