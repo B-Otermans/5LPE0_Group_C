c77ee7e979fc0b808b8bfb00b861197673264b1e
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2024-12-15 14:46:18 +0100

    multiport extractor werkt

---
+++ analysis_controls.py
@@ -0,0 +1,15 @@
+import simulate
+import imp
+
+# force module updates
+CUSTOM_MODULES = [simulate]
+for module in CUSTOM_MODULES:
+    imp.reload(module)
+
+# extraction paramters
+SIMULATION_NAME = "Fractionated Dipole Array simulation at 298MHz"
+NORMALIZED_POWER = 8.0  # power in watts to be divided over antennas
+
+# if-statement to only perform extraction when this file is run directly
+if __name__ == "__main__":
+    simulate.extract_multiport(simulation_name=SIMULATION_NAME, normalized_power=NORMALIZED_POWER)
