f6635c1bd077624ecdb661d7d41c731120824d8f
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2025-01-25 20:53:02 +0100

    single extractor toegevoegd

--- analysis_controls.py
+++ analysis_controls.py
@@ -13,3 +13,4 @@ NORMALIZED_POWER = 8.0  # power in watts to be divided over antennas
 # if-statement to only perform extraction when this file is run directly
 if __name__ == "__main__":
     simulate.extract_multiport(simulation_name=SIMULATION_NAME, normalized_power=NORMALIZED_POWER)
+    simulate.extract_singleports(simulation_name=SIMULATION_NAME, relative_path="EXPORTS")
